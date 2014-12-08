30cb1539f475c503c6b954d3ce5f8d8031e72254
lbhasemeyer <lbhasemeyer@gmail.com>
2014-12-08 15:48:48 -0700

    finished exercises

--- 04_all_employments.rb
+++ 04_all_employments.rb
@@ -171,3 +171,10 @@ crm = {
 #   Jane Otto
 #
 # ------- your code under here -----------
+
+crm[:people].each do |person|
+  puts "#{person[:first_name]} #{person[:last_name]}"
+  person[:employments].each do |employment|
+    puts "   #{employment[:title]} #{employment[:company_id]}"
+  end
+end
